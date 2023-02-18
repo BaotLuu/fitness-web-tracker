3ced5cfb742cecf1df85e2a684bff4157fa1edec
J-P <enqz99@gmail.com>
2023-02-18 17:25:15 -0500

    FAQ update

--- frontend/src/pages/FAQ.js
+++ frontend/src/pages/FAQ.js
@@ -1,11 +1,76 @@
 import React from "react";
 
 function FAQ() {
-  return (
-    <div>
-      <h1>PUT FAQ text HERE</h1>
-    </div>
-  );
+return (
+<div>
+<h1>Fitness Tracker Web Application FAQ:</h1>
+<h2>1. General:</h2>
+<ul>
+<li>
+<strong>Is the fitness tracker app free to use?</strong>
+<br />
+The app offers a free version with basic features, as well as a premium version with more advanced features for a monthly or annual fee.
+</li>
+<li>
+<strong>Can I use the app on multiple devices?</strong>
+<br />
+Yes, the app can be used on multiple devices by logging in with the same account on each device.
+</li>
+</ul>
+<h2>2. Exercises:</h2>
+<ul>
+<li>
+<strong>How do I track my exercises on the app?</strong>
+<br />
+The app allows you to log your exercises manually or by using the built-in exercise tracker. Simply select the exercise you performed, the duration and the date, and the app will do the rest.
+</li>
+<li>
+<strong>Can I add custom exercises to the list?</strong>
+<br />
+Yes, you can add custom exercises to the app by entering the name and other details.
+</li>
+</ul>
+<h2>3. Goals:</h2>
+<ul>
+<li>
+<strong>How do I set my fitness goals on the app?</strong>
+<br />
+You can set your fitness goals by going to the "Goals" section in the app and entering your desired goal, such as the number of calories you want to burn or the number of steps you want to take.
+</li>
+<li>
+<strong>Can I track my progress towards my goals?</strong>
+<br />
+Yes, the app allows you to track your progress towards your goals by displaying your progress in a visual way, such as a progress bar or a graph.
+</li>
+</ul>
+<h2>4. Journals:</h2>
+<ul>
+<li>
+<strong>What is a journal on the fitness tracker app?</strong>
+<br />
+A journal on the fitness tracker app is a place where you can log your thoughts, feelings, and experiences related to your fitness journey.
+</li>
+<li>
+<strong>How do I add entries to my journal?</strong>
+<br />
+You can add entries to your journal by going to the "Journals" section in the app and typing in your thoughts, feelings, and experiences.
+</li>
+</ul>
+<h2>5. Security:</h2>
+<ul>
+<li>
+<strong>Is my data on the app secure?</strong>
+<br />
+Yes, the app takes the security of your data very seriously and uses industry-standard encryption methods to protect your information.
+</li>
+<li>
+<strong>What happens if I lose access to my account?</strong>
+<br />
+If you lose access to your account, you can use the app's account recovery feature to regain access. Simply follow the steps outlined in the account recovery process.
+</li>
+</ul>
+</div>
+);
 }
 
 export default FAQ;
